38ecfa8c2965a913c66513b5447f10200871fd43
azeef-dev <azeefashraf22@gmail.com>
2026-09-18 00:09:36 -0700

    refactor: simplify App component and remove toast utility functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,9 @@
-import { Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Assets from "./pages/Assets";
-import Issues from "./pages/Issues";
-import AssetPublicPage from "./pages/AssetPublicPage";
-import ProtectedRoute from "./components/ProtectedRoute";
+import React from 'react'
 
 function App() {
   return (
-    <>
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        gutter={12}
-        containerStyle={{ top: 20 }}
-      />
-
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/assets"
-          element={
-            <ProtectedRoute>
-              <Assets />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/issues"
-          element={
-            <ProtectedRoute>
-              <Issues />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/asset/:assetCode" element={<AssetPublicPage />} />
-      </Routes>
-    </>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
